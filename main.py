f8a5cd6ca48ffa738b03f8a1639af44da2b121f8
Ethan <elnair122@gmail.com>
2023-03-27 05:35:21 -0400

    player rectangle created

--- main.py
+++ main.py
@@ -10,18 +10,29 @@ pygame.display.set_caption("Space Dodge")
 
 BG = pygame.transform.scale(pygame.image.load("bg.jpeg"), (WIDTH, HEIGHT))
 
-def draw():
+PLAYER_WIDTH = 40
+PLAYER_HEIGHT = 60
+
+
+def draw(player):
     WIN.blit(BG, (0, 0))
+
+    pygame.draw.rect(WIN, "red", player)
+   
     pygame.display.update()
 
 def main():
     run = True
+
+    player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
+   
+
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
-        draw()
+        draw(player)
     pygame.quit()
 
 if __name__ == "__main__":
